b0e8332ad650b5b24cedd88060e97b837ff8b59a
Sebastian Freundt <freundt@ga-group.nl>
2018-05-08 01:43:06 +0000

    ego, update copyright years

--- src/sex.c
+++ src/sex.c
@@ -1,6 +1,6 @@
 /*** sex.c -- simulating executions
  *
- * Copyright (C) 2016-2017 Sebastian Freundt
+ * Copyright (C) 2016-2018 Sebastian Freundt
  *
  * Author:  Sebastian Freundt <freundt@ga-group.nl>
  *
